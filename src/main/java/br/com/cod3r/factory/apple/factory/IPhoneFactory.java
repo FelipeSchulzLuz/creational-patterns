cd34bc8604d478eecabd7112b96b013aa078dc8f
Felipe Schulz <36828158+FelipeSchulzLuz@users.noreply.github.com>
2023-03-15 21:33:01 -0300

    first commit

---
+++ src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.factory.apple.factory;
+
+public abstract class IPhoneFactory {
+
+    abstract IPhone orderIPhone11() {}
+
+    abstract IPhone orderIPhone11Pro() {}
+
+    abstract IPhone orderIPhoneX() {}
+    
+    abstract IPhone orderIPhoneXSMax() {}
+
+}

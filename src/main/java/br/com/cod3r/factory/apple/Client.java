6e6ea40fc6a012b8a4a4709e2cfb9e6641f49c9c
Felipe Schulz <36828158+FelipeSchulzLuz@users.noreply.github.com>
2023-03-16 21:55:19 -0300

    Feature - Implementing enum with factories

--- src/main/java/br/com/cod3r/factory/apple/Client.java
+++ src/main/java/br/com/cod3r/factory/apple/Client.java
@@ -1,18 +1,17 @@
 package br.com.cod3r.factory.apple;
 
 import br.com.cod3r.factory.apple.enums.IPhoneType;
-import br.com.cod3r.factory.apple.model.IPhone;
 
 public class Client {
-	
+
 	public static void main(String[] args) {
 		IPhoneType iphoneX = IPhoneType.IPHONE_X;
 		IPhoneType iphone11Pro = IPhoneType.IPHONE_11_PRO;
 
 		System.out.println("### Ordering an iPhone X");
-		IPhone phonex = iphoneX.orderPhone();
+		iphoneX.orderPhone();
 
 		System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
-		IPhone iphone11 = iphone11Pro.orderPhone();
+		iphone11Pro.orderPhone();
 	}
 }

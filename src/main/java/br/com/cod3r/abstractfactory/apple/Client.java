a481bb6a1b8a586b32bed9e8d2eb24471d1be695
Felipe Schulz <36828158+FelipeSchulzLuz@users.noreply.github.com>
2023-03-29 21:03:01 -0300

    implemening concrete classes with CountryRulesAbstractFactory into existing Factory

--- src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
+++ src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
@@ -1,8 +1,25 @@
 package br.com.cod3r.abstractfactory.apple;
 
+import br.com.cod3r.abstractfactory.apple.factory.IPhone11Factory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneFactory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneXFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.BrazilianRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.CountryRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.UsRulesAbstractFactory;
+
 public class Client {
 	
 	public static void main(String[] args) {
+		CountryRulesAbstractFactory brazilianRules = new BrazilianRulesAbstractFactory();
+		CountryRulesAbstractFactory usRules = new UsRulesAbstractFactory();
+
+		System.out.println("### Ordering an iPhone 11 in Brazil");
+		IPhoneFactory iphone11BrazilFactory = new IPhone11Factory(brazilianRules);
+		iphone11BrazilFactory.orderIPhone("standard");
+
+		System.out.println("### Ordering an iPhone XS Max in USA");
+		IPhoneFactory iphoneXUsFactory = new IPhoneXFactory(usRules);
+		iphoneXUsFactory.orderIPhone("highEnd");
 
 	}
 }
